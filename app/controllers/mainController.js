eb3ecdc6e6d783230c74adb34a83290764f9b33e
LaetitiaChallant <laetitia.challant@oclock.school>
2025-02-12 10:05:21 +0100

    refactor: add try/catch

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,6 +1,11 @@
 import dataMapper from "../dataMapper.js";
 
 export const renderHomePage = async (req, res) => {
-  const coffees = await dataMapper.getAllCoffees();
-  res.render('home', {coffees});
+  try {
+    const coffees = await dataMapper.getAllCoffees();
+    res.render('home', {coffees});
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Une erreur s'est produite.");
+  }
 };
--- app/controllers/shopController.js
+++ app/controllers/shopController.js
@@ -1,11 +1,21 @@
 import dataMapper from "../dataMapper.js";
 
 export const  renderShopPage = async (req, res) => {
-  const coffees = await dataMapper.getAllCoffees();
-  res.render('shop', {coffees});
+  try {
+    const coffees = await dataMapper.getAllCoffees();
+    res.render('shop', {coffees});
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Une erreur s'est produite");    
+  }
 };
 export const renderArticlePage = async (req, res) => {
-  const coffeeId = parseInt(req.params.id);
-  const coffee = await dataMapper.getOneCoffee(coffeeId);
-  res.render('article', {coffee});
+  try {
+    const coffeeId = parseInt(req.params.id);
+    const coffee = await dataMapper.getOneCoffee(coffeeId);
+    res.render('article', {coffee});
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Une erreur s'est produite");    
+  }
 }; 
