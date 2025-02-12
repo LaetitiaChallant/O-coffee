6a1c28584e99c128761737ccf34d86a4602d2525
LaetitiaChallant <laetitia.challant@oclock.school>
2025-02-12 09:58:09 +0100

    add: boosting home page

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,3 +1,6 @@
-export const renderHomePage = (req, res) => {
-  res.render('home');
+import dataMapper from "../dataMapper.js";
+
+export const renderHomePage = async (req, res) => {
+  const coffees = await dataMapper.getAllCoffees();
+  res.render('home', {coffees});
 };
